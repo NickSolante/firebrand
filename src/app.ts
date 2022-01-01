4b8d8e945ba1bdb46613cd6b73c469d4f022f957
NickSolante <41457938+NickSolante@users.noreply.github.com>
2022-01-01 16:35:56 +1100

    chore: push temporary hiatus

--- src/app.ts
+++ src/app.ts
@@ -2,36 +2,53 @@ import express from "express";
 import bp from "body-parser";
 import qr from "qrcode";
 import cors from "cors";
+import { createConnection, getRepository } from "typeorm";
+import { QrCode } from "./entity/QrCode";
 
-const app = express();
-const port = process.env.PORT || 3000;
+const qrCodeCreation = async (url: string): Promise<string> => {
+  return qr
+    .toDataURL(url)
+    .then((src) => {
+      return src;
+    })
+    .catch((e) => {
+      throw e;
+    });
+};
 
-app.set("view engine", "ejs");
-app.use(cors());
-app.use(bp.urlencoded({ extended: false }));
-app.use(bp.json());
+createConnection().then((connection) => {
+  const app = express();
+  const port = process.env.PORT || 3000;
 
-app.get("/qrCode", (req, res) => {
-  res.send("hello world!");
-});
+  const qrCodeRepo = connection.getRepository(QrCode);
 
-app.post("/scan", (req, res) => {
-  const url = req.body.url;
+  app.set("view engine", "ejs");
+  app.use(cors());
+  app.use(bp.urlencoded({ extended: false }));
+  app.use(bp.json());
 
-  // If the input is null return "Empty Data" error
-  if (url.length === 0) res.send("Empty Data!");
-
-  // Let us convert the input stored in the url and return it as a representation of the QR Code image contained in the Data URI(Uniform Resource Identifier)
-  // It shall be returned as a png image format
-  // In case of an error, it will save the error inside the "err" variable and display it
-
-  qr.toDataURL(url, (err, src) => {
-    if (err) res.send("Error occured");
-    // Let us return the QR code image as our response and set it to be the source used in the webpage
-    res.send(src);
+  app.get("/qrCode", (req, res) => {
+    res.send("hello world!");
   });
-});
 
-app.listen(port, () => {
-  console.log(`example app is listening to ${port}`);
+  app.post("/qrCode", async (req, res) => {
+    const url = req.body.url;
+
+    // If the input is null return "Empty Data" error
+    if (url.length === 0) {
+      res.send("Empty Data!");
+    } else {
+      const code = await qrCodeCreation(url);
+      const qrCode = qrCodeRepo.create({ url, binaryImage: code });
+      await qrCodeRepo.save(qrCode);
+      res.send(code);
+    }
+
+    // Let us convert the input stored in the url and return it as a representation of the QR Code image contained in the Data URI(Uniform Resource Identifier)
+    // It shall be returned as a png image format
+    // In case of an error, it will save the error inside the "err" variable and display it
+  });
+  app.listen(port, () => {
+    console.log(`example app is listening to ${port}`);
+  });
 });
